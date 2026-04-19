3b4f8f09b1aaadf0376701d5097d07a6e7de06f2
Mehmet Fatih <meh.fatih77@gmail.com>
2026-04-19 19:34:10 +0300

    chore: remove .next cache and tsbuildinfo from tracking

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"W8NiUm4Vo3Yie5ut5D9iITLxDFzUarAPl4BiJf7+W1s=\"\n}"
--- .next/dev/static/chunks/app_[locale]_(auth)_signup_page_tsx_06vxdv8._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_05vutlj._.js","static/chunks/_0v9dcd4._.js"],
-    source: "dynamic"
-});
